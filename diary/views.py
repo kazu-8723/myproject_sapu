6b43bb1314945240c2f0e6690be552501ba48fe0
Matsuta Kazuya <mttkzy.8723@gmail.com>
2025-10-17 14:33:35 +0900

    トップページのView(リクエストを受け取り、レスポンスを作って返す)の作成

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+#汎用的なView用の基底クラス
+from django.views import View
 
-# Create your views here.
+class IndexView(View):
+    #request:ユーザから送られてきたデータを含むリクエストオブジェクト
+    #日記アプリのトップページにアクセスした時に動くメソッド
+    def get(self, request):
+        #render:画面のレスポンスを返す
+        #diary/index.html:返す画面のHTMLファイル
+        return render(request, "diary/index.html")
+
+#関数に変換
+index = IndexView.as_view()
